b1cbd7692a4f5191fa2407dcf5db5fb58e462e53
David Shirley <dshirley@crittercism.com>
2016-04-07 12:57:44 -0700

    Fix compiler warning with using www.data

--- DemoApp/Assets/CrittercismTestGUI.cs
+++ DemoApp/Assets/CrittercismTestGUI.cs
@@ -145,7 +145,7 @@ public class CrittercismTestGUI : MonoBehaviour
         yield return www;
         // check for errors
         if (www.error == null) {
-            Debug.Log ("WWW Ok!: " + www.data);
+            Debug.Log ("WWW Ok!: " + www.text);
         } else {
             Debug.Log ("WWW Error: " + www.error);
         }    
